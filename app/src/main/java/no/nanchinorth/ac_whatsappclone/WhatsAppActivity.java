2d68cdfb4ca8d3b32fd0b7b3b97330ad842228c4
bitmadao <dag@pepmagic.no>
2019-11-29 14:41:51 +0100

    Add activities for SignUp and somewhere to go when there is a logged in ParseUser.

---
+++ app/src/main/java/no/nanchinorth/ac_whatsappclone/WhatsAppActivity.java
@@ -0,0 +1,14 @@
+package no.nanchinorth.ac_whatsappclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WhatsAppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_whats_app);
+    }
+}
