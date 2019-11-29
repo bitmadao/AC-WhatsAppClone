21e804d0a311442950c5f3cfcf4fa2b04283484d
bitmadao <dag@pepmagic.no>
2019-11-29 19:56:57 +0100

    Add menu to WhatsAppActivity, add Logout functionality from ACWACHelperTools

--- app/src/main/java/no/nanchinorth/ac_whatsappclone/WhatsAppActivity.java
+++ app/src/main/java/no/nanchinorth/ac_whatsappclone/WhatsAppActivity.java
@@ -1,14 +1,72 @@
 package no.nanchinorth.ac_whatsappclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
-public class WhatsAppActivity extends AppCompatActivity {
+import com.parse.ParseUser;
+
+import static no.nanchinorth.ac_whatsappclone.ACWACHelperTools.*;
+
+public class WhatsAppActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private ParseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_whats_app);
+
+        if(ParseUser.getCurrentUser() != null){
+            currentUser = ParseUser.getCurrentUser();
+            setTitle(String.format("Conversations: %s", currentUser.getUsername()));
+        } else {
+            transitionToLogin();
+        }
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_whats_app_activity,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.menuItemWhatsAppActivityLogout:
+                logoutParseUser(WhatsAppActivity.this,this);
+                break;
+
+            case R.id.menuItemWhatsAppActivityChat:
+
+                break;
+
+            case R.id.menuItemWhatsAppActivityUserDirectory:
+
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    private void transitionToLogin(){
+        startActivity(new Intent(WhatsAppActivity.this,LoginActivity.class));
+        finish();
     }
 }
