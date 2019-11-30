c877f5807145409cf4eee60f8fe37d78d51d8ac0
bitmadao <dag@pepmagic.no>
2019-11-30 14:22:54 +0100

    Add code to hide keyboard for use in activities such as Login and SignUp

--- app/src/main/java/no/nanchinorth/ac_whatsappclone/ACWACHelperTools.java
+++ app/src/main/java/no/nanchinorth/ac_whatsappclone/ACWACHelperTools.java
@@ -2,6 +2,10 @@ package no.nanchinorth.ac_whatsappclone;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,7 +13,25 @@ import com.parse.LogOutCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 
+import static android.content.Context.INPUT_METHOD_SERVICE;
+
 public class ACWACHelperTools {
+
+    public static void hideSoftKeyboard(Context context, View activityLayout) {
+
+        try {
+            InputMethodManager inputMethodManager = (InputMethodManager) context.getSystemService(INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(activityLayout.getWindowToken(), 0);
+        } catch(Exception e) {
+            Log.i("APPTAG",e.getMessage());
+            Toast.makeText(
+                    context,
+                    context.getString(R.string.toast_generic_error),
+                    Toast.LENGTH_LONG)
+                    .show();
+        }
+
+    }
 
     public static void logoutParseUser(final Context context, final AppCompatActivity activity){
         ParseUser.logOutInBackground(new LogOutCallback() {
